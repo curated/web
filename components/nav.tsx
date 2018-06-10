3c54390d9aad2c2940f125dce5f249f2240066e8
Wagner Camarao <wagner@camarao.io>
2018-06-10 23:48:34 +1000

    Cleanup

--- components/nav.tsx
+++
@@ -1,9 +0,0 @@
-import './nav.scss'
-
-export default ({ visible }) => (
-  <nav className={`nav ${visible ? '' : 'hidden'}`}>
-    <section className="nav-container">
-      <h2>Sort by</h2>
-    </section>
-  </nav>
-)
--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/layout'
-import IssueCard from '../components/issue-card'
-
-export default () => (
-  <Layout>
-    <h1>Curated GitHub Issues</h1>
-    <IssueCard />
-  </Layout>
-)
