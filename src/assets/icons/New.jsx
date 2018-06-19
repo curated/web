e2177e646fcc1eb026a29d98cfc2206183fdf094
Wagner Camarao <wagner@camarao.io>
2018-06-19 23:36:14 +1000

    Update assets

--- src/assets/icons/New.jsx
+++ src/assets/icons/New.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
 const New = () => (
-  <svg viewBox="0 0 16 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <g stroke="none" strokeWidth="1" fillRule="evenodd">
-      <g transform="translate(-15.000000, -16.000000)">
-        <g transform="translate(15.000000, 16.000000)">
-          <g>
-            <path
-              d="M9,7 L14.5,7 L9,1.5 L9,7 Z M2,0 L10,0 L16,6 L16,18 C16,19.1 15.1,20 14,20 L2,20 C0.89,20 0,19.1 0,18 L0,2 C0,0.89 0.89,0 2,0 Z M11,16 L11,14 L2,14 L2,16 L11,16 Z M14,12 L14,10 L2,10 L2,12 L14,12 Z"
-              fillRule="nonzero"
-            />
-          </g>
-        </g>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 18"
+    version="1.1"
+    height="24px">
+    <g transform="translate(-15.000000, -17.000000)" fill="#ECEFF1">
+      <g transform="translate(15.000000, 17.000000)">
+        <path d="M2,0 L10,0 L16,5.4 L16,16.2 C16,17.19 15.1,18 14,18 L2,18 C0.89,18 0,17.19 0,16.2 L0,1.8 C0,0.801 0.89,0 2,0 Z M9,6.3 L14.5,6.3 L9,1.35 L9,6.3 Z M11,14.4 L11,12.6 L2,12.6 L2,14.4 L11,14.4 Z M14,10.8 L14,9 L2,9 L2,10.8 L14,10.8 Z" />
       </g>
     </g>
   </svg>
