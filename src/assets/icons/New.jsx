7eda9d13ff8935f416530b60affffa7efdcafe82
Wagner Camarao <wagner@camarao.io>
2018-06-20 00:40:51 +1000

    Lighten header and footer

--- src/assets/icons/New.jsx
+++ src/assets/icons/New.jsx
@@ -7,7 +7,7 @@ const New = () => (
     version="1.1"
     role="img"
     height="24px">
-    <g transform="translate(-15.000000, -17.000000)" fill="#ECEFF1">
+    <g transform="translate(-15.000000, -17.000000)" fill="#3f4e56">
       <g transform="translate(15.000000, 17.000000)">
         <path d="M2,0 L10,0 L16,5.4 L16,16.2 C16,17.19 15.1,18 14,18 L2,18 C0.89,18 0,17.19 0,16.2 L0,1.8 C0,0.801 0.89,0 2,0 Z M9,6.3 L14.5,6.3 L9,1.35 L9,6.3 Z M11,14.4 L11,12.6 L2,12.6 L2,14.4 L11,14.4 Z M14,10.8 L14,9 L2,9 L2,10.8 L14,10.8 Z" />
       </g>
